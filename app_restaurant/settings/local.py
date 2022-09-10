b9d298ea63c9ad68e81f2f835f7686c4e0e4b13b
Robert Tolentino <robert.tm2018@gmail.com>
2022-09-10 16:51:31 -0500

    Apply good practices

---
+++ app_restaurant/settings/local.py
@@ -0,0 +1,34 @@
+from .base import *  # noqa: F403
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xdyfkcj64i@(#a$hevbc0l*e(!6**kfxwnadkzlsce5kha-=bc'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+# Permite crear una lista para indicar que IP's se pueden conectar a nuestro aplicativo.
+# * : Permite que cualquier IP se pueda conectar.
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_restaurant',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',  # 127.0.0.1
+        'PORT': 5433,
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static',  # noqa: F405
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # noqa: F405
